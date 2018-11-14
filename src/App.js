68adbe65be1b0cdd2ce9673d7a0cd4e41bdf0863
Marek Reid <marekreid@Mareks-MacBook-Pro.local>
2018-11-14 07:57:56 +0100

    just need the image to show now-still cant get it to pull down from the server wth the id....

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://api-euwest.graphcms.com/v1/cjognjrxw06cv01fuou0lt1ss/master'
+});
+
+const POST_QUERY = gql`
+{
+  clientupdateses {
+    title
+    recentupdates
+    screenshot {
+      id
+    }
+    createdAt
+  }
+  }
+`
+
+
+// client.query({
+//   query: testQuery
+// }).then(res => console.log(res))
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
+
+      
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Query query={POST_QUERY}>
+            {({loading, data}) => {
+              if(loading) return "Loading..."
+              const { clientupdateses } = data
+              return clientupdateses.map(clientupdateses => 
+                <div>
+              <h1>{clientupdateses.title}</h1>
+              <h1>{clientupdateses.recentupdates}</h1>
+              <div>{clientupdateses.createdAt}</div>
+              <img src={data.clientupdateses.screenshot} alt='some value'/>
+              </div>
+              
+                )
+            }}
+          </Query>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,6 +61,7 @@ class App extends Component {
           </a>
         </header>
       </div>
+      </ApolloProvider>
     );
   }
 }
